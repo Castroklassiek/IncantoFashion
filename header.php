503b8aeab71f80a20bc38fbd34755efd9f470558
Leslie Moreira de Castro <Castroklassiek@gmail.com>
2019-09-11 17:56:45 +0200

    ïnitial commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="./images/favicon-16x16.png">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>INCANTO</title>
+    <script type="text/javascript">
+    function show_alert (){
+        alert ("U wordt nu doorverwezen... Een moment geduld alstublieft.");
+    }
+    </script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg ">
+  <a class="navbar-brand" href="#">
+    <img src="./images/logo.jpg" width="280" height="130" class="d-inline-block align-top" alt="">
+  </a>
+    </nav>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php include "header.php"; ?>
+
+
+<div id="music">
+<audio autoplay="true" loop="true" src="./song/Michael%20Jackson%20Liberian%20Girl.mp3">
+</audio>
+</div>
+<main role="main" class="container">
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="container-fluid padding">
+                <div class="row padding">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="card-img-top" src="./images/foto1.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Dressing gown Tender Roses</h5>
+                                <p class="card-text">€ 69.90</p>
+                                <a href="#" onclick="show_alert()" class="btn btn-dark">Bestel nu</a>
+                            </div>
+                        </div>
+                    </div>
+                
+                
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="card-img-top" src="./images/foto2.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Short sleeve dressing gown</h5>
+                                <p class="card-text">€ 79.90</p>
+                                <a href="#" onclick="show_alert()" class="btn btn-dark">Bestel nu</a>
+                            </div>
+                        </div>
+                    </div>
+                
+                
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="card-img-top" src="./images/foto0.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Perseus Lace Robe Scarlett</h5>
+                                <p class="card-text">€ 89.90</p>
+                                <a href="#" onclick="show_alert()" class="btn btn-dark">Bestel nu</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- /.container -->
+
+
+<?php include "footer.php"; ?>
